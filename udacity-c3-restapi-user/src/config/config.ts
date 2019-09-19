6c6368e0047246f3f21234bb8f3f39f4edc77ef3
ndakena <ndakena@gmail.com>
2019-09-19 19:03:33 +0100

    innitial project

---
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -0,0 +1,24 @@
+export const config = {
+  "dev": {
+    "username": "postgres",
+    "password": "root",
+    "database": "udagram_dev",
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET,
+    "url": process.env.URL
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  }
+
+}
